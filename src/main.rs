df1955d7ec908504358b11e09ea7b4604041fc83
balask-dev <balask1225@gmail.com>
2024-06-23 08:57:16 +0530

    first

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io::stdin;
+fn main() {
+    println!("Hello, world!");
+    //create var's
+    let mut valueOne:String = String::new();
+    let mut valueTwo:String = String::new();
+    let mut value:String    = String::new();
+
+    println!("Enter valueOne : ");
+    stdin().read_line(&mut valueOne).expect("please check your value");
+
+    
+}
---
+++ target/debug/deps/calc-235b7b22dfd2afc2.d
@@ -0,0 +1,5 @@
+/home/razorb/Documents/Rust_projects/calc/calc/target/debug/deps/calc-235b7b22dfd2afc2.rmeta: src/main.rs
+
+/home/razorb/Documents/Rust_projects/calc/calc/target/debug/deps/calc-235b7b22dfd2afc2.d: src/main.rs
+
+src/main.rs:
